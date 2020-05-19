eae78797d64505c0327591049ebc9fec55a4934a
Varun Jain <varun.jain@tala.co>
2020-05-19 14:51:06 +0530

    Sample Espresso Project

---
+++ app/src/androidTest/java/com/learn/espresso/annotations/DummyAnnotation.kt
@@ -0,0 +1,7 @@
+package com.learn.espresso.annotations
+
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DummyAnnotation {
+
+}
